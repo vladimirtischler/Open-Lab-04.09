063568269e4159d9db43a931eae061d664168a67
vladimirtischler <tischler.vladimir@gmail.com>
2019-12-08 21:57:19 +0100

    asc

--- Open-Lab-04.09/ArrayTools.cs
+++ Open-Lab-04.09/ArrayTools.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Open_Lab_04._09
 {
@@ -6,7 +9,15 @@ namespace Open_Lab_04._09
     {
         public string[] RemoveDups(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> singleWords = new List<string>();
+            foreach (var c in strings)
+            {
+                if (!singleWords.Contains(c))
+                {
+                    singleWords.Add(c);
+                }
+            }
+            return singleWords.ToArray();
         }
     }
 }
